9a0bc810c1292e1b9fdd2aad9a817642681e39fe
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    ver.1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,21 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+        List<User> userList = new ArrayList<>();
+
+        userService.createUsersTable();
+        userService.saveUser("Ivan", "Ivanov", (byte) 20);
+        userService.saveUser("Pavel", "Pavlov", (byte) 21);
+        userService.saveUser("Petr", "Petrov", (byte) 32);
+        userList = userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+    private final static String URL = "jdbc:mysql://localhost:3306/users";
+    private final static String LOGIN = "root";
+    private final static String PASSWORD = "78877887";
+    private static Connection connection;
+
+    public static void createNewConnection() throws SQLException {
+        connection = DriverManager.getConnection(URL, LOGIN, PASSWORD);
+        System.out.println("Соединение установлено");
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                createNewConnection();
+            } catch (SQLException e) {
+
+            }
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+
+        }
+    }
 }
